87ac3a39793689c93aab8e003d2f705d4f4e9686
Martin Mackovič <xmackov1@fi.muni.cz>
2025-09-30 14:23:39 +0200

    add author, order, order item, publisher, user

---
+++ BookHub/DAL/Models/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+	public class OrderItem: BaseEntity
+	{
+		public int Quantity { get; set; }
+
+		public int OrderId { get; set; }
+
+		public int BookId { get; set; }
+
+		[ForeignKey(nameof(OrderId))]
+		public virtual Order? Order { get; set; }
+	}
+}
---
+++ BookHub/DAL/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+	public class User: BaseEntity
+	{
+		public string Username { get; set; }
+
+		public string Email { get; set; }
+
+		public string PasswordHash { get; set; }
+
+		public string PasswordSalt { get; set; }
+
+		public bool IsBanned { get; set; }
+	}
+}
