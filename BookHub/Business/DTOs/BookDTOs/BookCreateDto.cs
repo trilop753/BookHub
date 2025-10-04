22e3e865a29a67949025fc25f6bed9b2c17faeb7
Maroš Pavlík <xpavlik9@fi.muni.cz>
2025-10-04 19:38:50 +0200

    feat: add addition book dtos, add bookservice

---
+++ BookHub/Business/DTOs/BookDTOs/BookCreateDto.cs
@@ -0,0 +1,29 @@
+﻿using Business.DTOs.AuthorDTOs;
+using Business.DTOs.BookReviewDTOs;
+using Business.DTOs.GenreDTOs;
+using Business.DTOs.PublisherDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DTOs.BookDTOs
+{
+    public class BookCreateDto
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ISBN { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int PublisherId { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public IEnumerable<int> GenreIds { get; set; }
+    }
+}
--- BookHub/Business/Services/BookService.cs
+++ BookHub/Business/Services/BookService.cs
@@ -1,15 +1,24 @@
 ﻿using Business.DTOs.BookDTOs;
 using Business.Mappers;
 using Business.Services.Interfaces;
+using DAL.Models;
 using Infrastructure.Repository.Interfaces;
 
 public class BookService
 {
     private readonly IBookRepository _bookRepository;
+    private readonly IGenreRepository _genreRepository;
+    private readonly IAuthorRepository _authorRepository;
+    private readonly IPublisherRepository _publisherRepository;
 
-    public BookService(IBookRepository bookRepository)
+
+    public BookService(IBookRepository bookRepository, IGenreRepository genreRepository,
+        IAuthorRepository authorRepository, IPublisherRepository publisherRepository)
     {
         _bookRepository = bookRepository;
+        _genreRepository = genreRepository;
+        _authorRepository = authorRepository;
+        _publisherRepository = publisherRepository;
     }
 
     public async Task<BookDto?> GetBookByIdAsync(int id)
@@ -24,16 +33,30 @@ public class BookService
         return books.Select(b => b.MapToDto());
     }
 
-    //public async Task<BookDto> CreateBookAsync(BookDto dto)
-    //{
-    //    var entity = dto.MapToEntity();
-    //    await _bookRepository.AddAsync(entity);
-    //    await _bookRepository.SaveChangesAsync();
+    public async Task<BookDto> CreateBookAsync(BookCreateDto dto)
+    {
+        var author = await _authorRepository.GetByIdAsync(dto.AuthorId);
+        var publisher = await _publisherRepository.GetByIdAsync(dto.PublisherId);
+        var genres = await _genreRepository.GetAllByIdsAsync(dto.GenreIds.ToArray());
 
-    //    return entity.MapToDto();
-    //}
+        //TODO check null? (ids will be picked from a list?)
+        var newBook = new Book
+        {
+            Title = dto.Title,
+            Description = dto.Description,
+            Price = dto.Price,
+            Author = author,
+            Publisher = publisher,
+            Genres = genres.ToList()
+        };
 
-    public async Task<bool> UpdateBookAsync(BookDto dto)
+        await _bookRepository.AddAsync(newBook);
+        await _bookRepository.SaveChangesAsync();
+
+        return newBook.MapToDto();
+    }
+
+    public async Task<bool> UpdateBookAsync(BookUpdateDto dto)
     {
         var existing = await _bookRepository.GetByIdAsync(dto.Id);
         if (existing == null)
@@ -42,7 +65,14 @@ public class BookService
         existing.Title = dto.Title;
         existing.Description = dto.Description;
         existing.Price = dto.Price;
-        // TODO update genres,author,publisher ?
+
+        existing.AuthorId = dto.AuthorId;
+        existing.PublisherId = dto.PublisherId;
+
+        var genres = await _genreRepository.GetAllByIdsAsync(dto.GenreIds.ToArray());
+        //TODO check empty?
+        existing.Genres = genres.ToList();
+        // TODO update reviews - in ReviewService?
 
         await _bookRepository.SaveChangesAsync();
         return true;
