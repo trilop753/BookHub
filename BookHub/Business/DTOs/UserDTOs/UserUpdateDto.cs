fba7a58c84632a6754a841ff757cf1dceb79f018
Martin Mackovič <xmackov1@fi.muni.cz>
2025-10-05 19:37:57 +0200

    add UserUpdateDTO

---
+++ BookHub/Business/DTOs/UserDTOs/UserUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DTOs.UserDTOs
+{
+	public class UserUpdateDto
+	{
+		public int Id { get; set; }
+
+		public string Username { get; set; }
+
+		public string Email { get; set; }
+
+		public bool IsBanned { get; set; }
+	}
+}
