3168344f457515b286944e11fa6a423d0cfd61ef
Martin Mackovič <xmackov1@fi.muni.cz>
2025-10-05 18:41:31 +0200

    add documentation

--- BookHub/Business/DTOs/BookDTOs/BookSearchCriteriaDto.cs
+++ BookHub/Business/DTOs/BookDTOs/BookSearchCriteriaDto.cs
@@ -8,14 +8,25 @@ namespace Business.DTOs.BookDTOs
 {
 	public class BookSearchCriteriaDto
 	{
-		public string? Name { get; set; }
+		/// <summary>
+		/// Books that contain the provided title text in their Title.
+		/// Case insensitive.
+		/// </summary>
+		public string? Title { get; set; }
 
+		/// <summary>
+		/// Books that contain the provided description text in their Description.
+		/// Case insensitive.
+		/// </summary>
 		public string? Description { get; set; }
 
 		public decimal? LowPrice { get; set; }
 
 		public decimal? HighPrice { get; set; }
 
+		/// <summary>
+		/// Books that contain at least one of the provided genres.
+		/// </summary>
 		public int[]? GenreIds { get; set; }
 
 		public int? AuthorId { get; set; }
--- BookHub/Business/Services/BookService.cs
+++ BookHub/Business/Services/BookService.cs
@@ -36,7 +36,7 @@ public class BookService : IBookService
 
 	public async Task<IEnumerable<BookSummaryDto>> GetFiltered(BookSearchCriteriaDto searchCriteria)
 	{
-        var books = await _bookRepository.GetFiltered(searchCriteria.Name, searchCriteria.Description, 
+        var books = await _bookRepository.GetFiltered(searchCriteria.Title, searchCriteria.Description, 
             searchCriteria.LowPrice, searchCriteria.HighPrice, searchCriteria.GenreIds, 
             searchCriteria.AuthorId, searchCriteria.PublisherId
         );
