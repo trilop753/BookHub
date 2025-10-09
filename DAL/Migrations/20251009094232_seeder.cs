37ec3bade97d52a0d8c6f44c2ccdaeec0d5a6422
Martin Mackovič <xmackov1@fi.muni.cz>
2025-10-09 11:42:58 +0200

    add migration for seeder

---
+++ DAL/Migrations/20251009094232_seeder.cs
@@ -0,0 +1,284 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class seeder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "Name", "Surname" },
+                values: new object[,]
+                {
+                    { 3, "Stephen", "King" },
+                    { 4, "Jane", "Austen" },
+                    { 5, "Isaac", "Asimov" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "BookReview",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Body",
+                value: "Absolutely loved it! A masterpiece of fantasy.");
+
+            migrationBuilder.UpdateData(
+                table: "BookReview",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Body", "UserId" },
+                values: new object[] { "Creepy, atmospheric, and unique.", 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Description", "ISBN", "Price" },
+                values: new object[] { "Classic high fantasy adventure.", "9780547928210", 10.99m });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Description", "ISBN", "Price", "PublisherId" },
+                values: new object[] { "A collection of weird horror stories.", "9780486226886", 6.99m, 2 });
+
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 3, "Science Fiction" },
+                    { 4, "Romance" },
+                    { 5, "Thriller" },
+                    { 6, "Biography" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Penguin Books");
+
+            migrationBuilder.UpdateData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "HarperCollins");
+
+            migrationBuilder.InsertData(
+                table: "Publishers",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 3, "Bloomsbury" },
+                    { 4, "Vintage" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "Username" },
+                values: new object[] { "admin@bookhub.com", "admin" });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Email", "IsBanned", "Username" },
+                values: new object[] { "john.doe@gmail.com", false, "john" });
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "Id", "Email", "IsBanned", "Username" },
+                values: new object[,]
+                {
+                    { 3, "emma.reader@gmail.com", false, "emma" },
+                    { 4, "mike.writer@gmail.com", false, "mike" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "BookGenre",
+                columns: new[] { "BooksId", "GenresId" },
+                values: new object[] { 2, 3 });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AuthorId", "Description", "ISBN", "Price", "PublisherId", "Title" },
+                values: new object[,]
+                {
+                    { 3, 4, "A romantic novel of manners.", "9780141439518", 8.49m, 3, "Pride and Prejudice" },
+                    { 4, 5, "Science fiction classic about the fall of the Galactic Empire.", "9780553293357", 9.99m, 4, "Foundation" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "BookGenre",
+                columns: new[] { "BooksId", "GenresId" },
+                values: new object[,]
+                {
+                    { 3, 4 },
+                    { 4, 5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "BookReview",
+                columns: new[] { "Id", "Body", "BookId", "Stars", "UserId" },
+                values: new object[] { 3, "Classic romance with wit and heart.", 3, 5, 3 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "BookGenre",
+                keyColumns: new[] { "BooksId", "GenresId" },
+                keyValues: new object[] { 2, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "BookGenre",
+                keyColumns: new[] { "BooksId", "GenresId" },
+                keyValues: new object[] { 3, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "BookGenre",
+                keyColumns: new[] { "BooksId", "GenresId" },
+                keyValues: new object[] { 4, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "BookReview",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.UpdateData(
+                table: "BookReview",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Body",
+                value: "best book!");
+
+            migrationBuilder.UpdateData(
+                table: "BookReview",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Body", "UserId" },
+                values: new object[] { "nice", 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Description", "ISBN", "Price" },
+                values: new object[] { "you dont know lotr?", "1231231231231", 10m });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Description", "ISBN", "Price", "PublisherId" },
+                values: new object[] { "test description", "1231231231232", 5m, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "publisher1");
+
+            migrationBuilder.UpdateData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "publisher2");
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "Username" },
+                values: new object[] { "example@gmail.com", "trilop" });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Email", "IsBanned", "Username" },
+                values: new object[] { "example2@gmail.com", true, "username123" });
+        }
+    }
+}
