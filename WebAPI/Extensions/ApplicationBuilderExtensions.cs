8984323b9b9b5714fac70473f681053b5a39fffc
Adam Kala <xkala@fi.muni.cz>
2025-10-22 13:19:42 +0200

    Split Program.cs into extensions.

---
+++ WebAPI/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using DAL.Data;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Middlewares;
+
+namespace WebAPI.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static void ApplyMigrations<TContext>(this IApplicationBuilder app)
+            where TContext : DbContext
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<TContext>();
+            db.Database.Migrate();
+        }
+
+        public static IApplicationBuilder UseCorsPolicy(this IApplicationBuilder app)
+        {
+            return app.UseCors("AllowSwaggerUI");
+        }
+
+        public static IApplicationBuilder UseAppMiddlewares(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<LoggingMiddleware>();
+            app.UseMiddleware<AuthenticationMiddleware>();
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            return app;
+        }
+    }
+}
