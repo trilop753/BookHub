cdfbd587605490445c28d15d09c924ac2ed0ab5c
Adam Kala <xkala@fi.muni.cz>
2025-10-22 14:03:40 +0200

    Refactored middlewares order.

--- WebAPI/Extensions/ApplicationBuilderExtensions.cs
+++ WebAPI/Extensions/ApplicationBuilderExtensions.cs
@@ -4,29 +4,30 @@ using WebAPI.Middlewares;
 
 namespace WebAPI.Extensions
 {
-    public static class ApplicationBuilderExtensions
-    {
-        public static void ApplyMigrations<TContext>(this IApplicationBuilder app)
-            where TContext : DbContext
-        {
-            using var scope = app.ApplicationServices.CreateScope();
-            var db = scope.ServiceProvider.GetRequiredService<TContext>();
-            db.Database.Migrate();
-        }
+	public static class ApplicationBuilderExtensions
+	{
+		public static void ApplyMigrations<TContext>(this IApplicationBuilder app)
+			where TContext : DbContext
+		{
+			using var scope = app.ApplicationServices.CreateScope();
+			var db = scope.ServiceProvider.GetRequiredService<TContext>();
+			db.Database.Migrate();
+		}
 
-        public static IApplicationBuilder UseCorsPolicy(this IApplicationBuilder app)
-        {
-            return app.UseCors("AllowSwaggerUI");
-        }
+		public static IApplicationBuilder UseCorsPolicy(this IApplicationBuilder app)
+		{
+			return app.UseCors("AllowSwaggerUI");
+		}
 
-        public static IApplicationBuilder UseAppMiddlewares(this IApplicationBuilder app)
-        {
-            app.UseMiddleware<LoggingMiddleware>();
-            app.UseMiddleware<AuthenticationMiddleware>();
+		public static IApplicationBuilder UseAppMiddlewares(this IApplicationBuilder app)
+		{
+			app.UseHttpsRedirection();
 
-            app.UseHttpsRedirection();
-            app.UseAuthorization();
-            return app;
-        }
-    }
+			app.UseAuthorization();
+
+			app.UseMiddleware<LoggingMiddleware>();
+			app.UseMiddleware<AuthenticationMiddleware>();
+			return app;
+		}
+	}
 }
