677b2b611e712c6be260c3c6b134cada44b36303
Martin Mackovič <xmackov1@fi.muni.cz>
2025-10-22 23:29:20 +0200

    edit Result to include Success

--- Business/UtilClasses/Result.cs
+++ Business/UtilClasses/Result.cs
@@ -1,9 +1,8 @@
 ﻿namespace Business.UtilClasses
 {
-    public class Result<T>
+    public class Result
     {
         public bool Success { get; set; }
-        public T? Data { get; set; }
         public string? Error { get; set; }
     }
 }
