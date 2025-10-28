21ebbc0010dc0d953ef3ace911bc0f719c262002
Martin Mackovič <xmackov1@fi.muni.cz>
2025-10-28 20:50:23 +0100

    add Cart BL

---
+++ BL/Mappers/CartItemMapper.cs
@@ -0,0 +1,19 @@
+﻿using BL.DTOs.CartItemDTOs;
+using DAL.Models;
+
+namespace BL.Mappers
+{
+    public static class CartItemMapper
+    {
+        public static CartItemDto MapToDto(this CartItem item)
+        {
+            return new CartItemDto()
+            {
+                Id = item.Id,
+                User = item.User.MapToSummaryDto(),
+                Book = item.Book.MapToSummaryDto(),
+                Quantity = item.Quantity,
+            };
+        }
+    }
+}
