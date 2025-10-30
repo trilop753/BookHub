414324de9fce44d5263bfc96c0131a889c6ccf20
Adam Kala <xkala@fi.muni.cz>
2025-10-30 17:12:21 +0100

    Added IAuthorFacade, IGenreFacade and IPublisherFacade interfaces

---
+++ BL/Facades/Interfaces/IGenreFacade.cs
@@ -0,0 +1,14 @@
+﻿using BL.DTOs.GenreDTOs;
+using FluentResults;
+
+namespace BL.Facades.Interfaces
+{
+    public interface IGenreFacade
+    {
+        Task<Result<IEnumerable<GenreDto>>> GetAllAsync();
+        Task<Result<GenreDto>> GetByIdAsync(int id);
+        Task<Result<GenreDto>> CreateAsync(GenreCreateDto dto);
+        Task<Result> UpdateAsync(int id, GenreUpdateDto dto);
+        Task<Result> DeleteAsync(int id);
+    }
+}
