4a0e9ff4e879169cf1a91acee027492729a247ff
Adam Kala <xkala@fi.muni.cz>
2025-11-01 15:15:16 +0100

    Add LogsController with endpoint for retrieving recent API logs

---
+++ WebAPI/Controllers/LogsController.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LogsController : ControllerBase
+    {
+        private readonly LogDbContext _context;
+
+        public LogsController(LogDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetLogs()
+        {
+            var logs = await _context
+                .Logs.OrderByDescending(l => l.Timestamp)
+                .Take(100)
+                .ToListAsync();
+
+            return Ok(logs);
+        }
+    }
+}
