76d0332790d3e9eed7a667d8913b4cade4a419ef
Adam Kala <xkala@fi.muni.cz>
2025-11-01 15:14:33 +0100

    Add LogDbContext for storing API request logs

---
+++ DAL/Data/LogDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Data
+{
+    public class LogDbContext : DbContext
+    {
+        public LogDbContext(DbContextOptions<LogDbContext> options)
+            : base(options) { }
+
+        public DbSet<LogEntry> Logs { get; set; }
+    }
+}
