c2b01e174306acc3b4c5f55db6ccddc08e2e31ef
Adam Kala <xkala@fi.muni.cz>
2025-11-01 15:14:47 +0100

    Add initial migration for LogDbContext (CreateLogsTable)

---
+++ DAL/Migrations/LogDb/20251101134519_CreateLogsTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations.LogDb
+{
+    /// <inheritdoc />
+    public partial class CreateLogsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Method = table.Column<string>(type: "TEXT", nullable: false),
+                    Path = table.Column<string>(type: "TEXT", nullable: false),
+                    StatusCode = table.Column<int>(type: "INTEGER", nullable: false),
+                    DurationMs = table.Column<double>(type: "REAL", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Logs");
+        }
+    }
+}
