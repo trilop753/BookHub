43dc8e627743c3b524586d319e18ad1b29d77aa1
Adam Kala <xkala@fi.muni.cz>
2025-11-04 20:16:44 +0100

    Removed redundand test.

--- BL.Tests/Services/WishlistItemServiceTests.cs
+++ BL.Tests/Services/WishlistItemServiceTests.cs
@@ -79,64 +79,6 @@ namespace BL.Tests.Services
         }
 
         [Fact]
-        public async Task CreateWishlistItemAsync_ShouldFailIfUserOrBookNotProvided()
-        {
-            var repository = Substitute.For<IWishlistItemRepository>();
-
-            var createDto = new WishlistItemCreateDto { UserId = 0, BookId = 0 };
-
-            repository
-                .AddAsync(Arg.Any<WishlistItem>())
-                .Returns(call =>
-                {
-                    var entity = call.Arg<WishlistItem>();
-                    entity.Id = 1;
-                    entity.User = new User
-                    {
-                        Id = 0,
-                        Username = "Unknown",
-                        Cart = new List<CartItem>(),
-                    };
-                    entity.Book = new Book
-                    {
-                        Id = 0,
-                        Title = "Unknown",
-                        Description = "",
-                        ISBN = "",
-                        Price = 0,
-                        Genres = new List<Genre>(),
-                        Publisher = new Publisher { Id = 1, Name = "Placeholder" },
-                        Author = new Author
-                        {
-                            Id = 1,
-                            Name = "N/A",
-                            Surname = "",
-                        },
-                        Reviews = new List<BookReview>(),
-                    };
-                    return Task.CompletedTask;
-                });
-
-            repository.SaveChangesAsync().Returns(Task.CompletedTask);
-
-            var serviceProvider = _serviceProviderBuilder
-                .AddScoped<IWishlistItemRepository>(repository)
-                .Create();
-
-            using (var scope = serviceProvider.CreateScope())
-            {
-                var wishlistService =
-                    scope.ServiceProvider.GetRequiredService<IWishlistItemService>();
-
-                var result = await wishlistService.CreateWishlistItemAsync(createDto);
-
-                Assert.True(result.IsSuccess);
-                Assert.Equal(0, result.Value.User.Id);
-                Assert.Equal(0, result.Value.Book.Id);
-            }
-        }
-
-        [Fact]
         public async Task DeleteWishlistItemAsync_ShouldDelete()
         {
             var repository = Substitute.For<IWishlistItemRepository>();
