7a48798e5d63f7657f3968121ef8f3b980e8687e
Martin Mackovič <xmackov1@fi.muni.cz>
2025-11-16 21:02:04 +0100

    add controller for detail

---
+++ WebMVC/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using BL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using WebMVC.Mappers;
+
+namespace WebMVC.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var book = await _bookService.GetBookByIdAsync(id);
+
+            if (book.IsFailed)
+            {
+                return View("NotFound");
+            }
+
+            return View(book.Value.MapToDetailView());
+        }
+    }
+}
