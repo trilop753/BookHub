4b43f559449f4f92fc0a11d114f99d25cba05958
Maroš Pavlík <xpavlik9@fi.muni.cz>
2025-11-22 19:08:53 +0100

    feat: add BookCreateViewModel

---
+++ WebMVC/Models/Book/BookCreateViewModel.cs
@@ -0,0 +1,51 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebMVC.Models.Book
+{
+    public class BookCreateViewModel
+    {
+        [Required]
+        [Display(Name = "Book Title")]
+        [MaxLength(25)]
+        public string Title { get; set; } = string.Empty;
+
+        [Display(Name = "Description")]
+        [MaxLength(500)]
+        public string Description { get; set; } = string.Empty;
+
+        [Display(Name = "ISBN")]
+        [MinLength(10)]
+        [MaxLength(13)]
+        public string ISBN { get; set; } = string.Empty;
+
+        [Display(Name = "Price")]
+        [Required]
+        [Range(0, int.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Author")]
+        [Required]
+        public int AuthorId { get; set; }
+
+        [Display(Name = "Publisher")]
+        [Required]
+        public int PublisherId { get; set; }
+
+        [Display(Name = "Genres")]
+        [Required]
+        public List<int> GenreIds { get; set; } = new();
+
+        [Display(Name = "Cover image url")]
+        [Required]
+        [Url]
+        public string CoverImageUrl { get; set; } = string.Empty;
+
+        public IEnumerable<SelectListItem> Authors { get; set; } =
+            Enumerable.Empty<SelectListItem>();
+        public IEnumerable<SelectListItem> Publishers { get; set; } =
+            Enumerable.Empty<SelectListItem>();
+        public IEnumerable<SelectListItem> Genres { get; set; } =
+            Enumerable.Empty<SelectListItem>();
+    }
+}
