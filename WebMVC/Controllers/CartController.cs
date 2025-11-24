d8e9ec58ca23b302cded9e45b632717bed831fae
Maroš Pavlík <xpavlik9@fi.muni.cz>
2025-11-24 21:10:22 +0100

    feat: cart controller index, update, remove, cart index view

--- WebMVC/Controllers/CartController.cs
+++ WebMVC/Controllers/CartController.cs
@@ -4,6 +4,8 @@ using DAL.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebMVC.Mappers;
+using WebMVC.Models.Cart;
 
 namespace WebMVC.Controllers;
 
@@ -59,5 +61,66 @@ public class CartController : Controller
         }
 
         return RedirectToAction("Detail", "Book", new { id = bookId });
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var identityUser = await _userManager.GetUserAsync(User);
+        if (identityUser == null || identityUser.User == null)
+        {
+            return View("InternalServerError");
+        }
+
+        var res = await _cartFacade.GetCartItemsByUserIdAsync(identityUser.User.Id);
+        if (res.IsFailed)
+        {
+            return View("InternalServerError");
+        }
+        return View(res.Value.MapToView());
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> RemoveInCart(int cartItemId)
+    {
+        var res = await _cartFacade.DeleteCartItemByIdAsync(cartItemId);
+        if (res.IsFailed)
+        {
+            return View("InternalServerError");
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> Update(CartViewModel model)
+    {
+        var identityUser = await _userManager.GetUserAsync(User);
+        if (identityUser == null || identityUser.User == null)
+        {
+            return View("InternalServerError");
+        }
+        if (!ModelState.IsValid)
+        {
+            var res = await _cartFacade.GetCartItemsByUserIdAsync(identityUser.User.Id);
+            if (res.IsFailed)
+            {
+                return View("InternalServerError");
+            }
+            return View("Index", res.Value.MapToView());
+        }
+
+        foreach (var item in model.Items)
+        {
+            if (item.Quantity == 0)
+            {
+                await _cartFacade.DeleteCartItemByIdAsync(item.Id);
+            }
+            else
+            {
+                await _cartFacade.UpdateItemQuantityAsync(item.Id, item.Quantity);
+            }
+        }
+
+        return RedirectToAction(nameof(Index));
     }
 }
