d22647557e46e6dec14cbdfa604fb58c7be1e905
Maroš Pavlík <xpavlik9@fi.muni.cz>
2025-11-25 12:05:27 +0100

    fix: add remove button to wishlist view, add correct redirection to wishlist controller remove

--- WebMVC/Controllers/WishlistController.cs
+++ WebMVC/Controllers/WishlistController.cs
@@ -68,7 +68,7 @@ namespace WebMVC.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Remove(int bookId)
+        public async Task<IActionResult> Remove(int bookId, string? returnUrl)
         {
             var identityUser = await _userManager.GetUserAsync(User);
             if (identityUser == null || identityUser.User == null)
@@ -83,6 +83,11 @@ namespace WebMVC.Controllers
             if (res.IsFailed)
             {
                 return View("InternalServerError");
+            }
+
+            if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+            {
+                return LocalRedirect(returnUrl);
             }
 
             return RedirectToAction("Detail", "Book", new { id = bookId });
