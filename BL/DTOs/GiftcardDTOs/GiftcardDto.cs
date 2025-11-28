8b061041635f1a9ab329eebe194e3746e5204328
Adam Kala <adamkala07@gmail.com>
2025-11-28 17:23:44 +0100

    Created GiftcardDTOs

---
+++ BL/DTOs/GiftcardDTOs/GiftcardDto.cs
@@ -0,0 +1,16 @@
+namespace BL.DTOs.GiftcardDTOs
+{
+    public class GiftcardDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+        public decimal Amount { get; set; }
+
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+
+        public int TotalCodes { get; set; }
+        public int UsedCodes { get; set; }
+    }
+}
