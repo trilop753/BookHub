2005de88b5be7bf17b552354c4b0e1b52794184f
Adam Kala <adamkala07@gmail.com>
2025-11-28 17:23:56 +0100

    Created GiftcardFacade

---
+++ BL/Facades/GiftcardFacade.cs
@@ -0,0 +1,63 @@
+using BL.DTOs.GiftcardDTOs;
+using BL.Facades.Interfaces;
+using BL.Services.Interfaces;
+using FluentResults;
+
+namespace BL.Facades
+{
+    public class GiftcardFacade : IGiftcardFacade
+    {
+        private readonly IGiftcardService _giftcardService;
+
+        public GiftcardFacade(IGiftcardService giftcardService)
+        {
+            _giftcardService = giftcardService;
+        }
+
+        public async Task<Result<GiftcardSummaryDto>> CreateAsync(GiftcardCreateDto dto)
+        {
+            var result = await _giftcardService.CreateAsync(dto);
+            if (result.IsFailed)
+                return Result.Fail(result.Errors);
+
+            return Result.Ok(result.Value);
+        }
+
+        public async Task<Result> DeleteAsync(int id)
+        {
+            return await _giftcardService.DeleteAsync(id);
+        }
+
+        public async Task<Result<IEnumerable<GiftcardDto>>> GetAllAsync()
+        {
+            return await _giftcardService.GetAllAsync();
+        }
+
+        public async Task<Result<GiftcardSummaryDto>> GetByIdAsync(int id)
+        {
+            var result = await _giftcardService.GetByIdAsync(id);
+            if (result.IsFailed)
+                return Result.Fail(result.Errors);
+
+            return Result.Ok(result.Value);
+        }
+
+        public async Task<Result<GiftcardSummaryDto>> UpdateAsync(int id, GiftcardUpdateDto dto)
+        {
+            var result = await _giftcardService.UpdateAsync(id, dto);
+            if (result.IsFailed)
+                return Result.Fail(result.Errors);
+
+            return Result.Ok(result.Value);
+        }
+
+        public async Task<Result<GiftcardCodeValidationDto>> ValidateCodeAsync(string code)
+        {
+            var result = await _giftcardService.ValidateCodeAsync(code);
+            if (result.IsFailed)
+                return Result.Fail(result.Errors);
+
+            return Result.Ok(result.Value);
+        }
+    }
+}
