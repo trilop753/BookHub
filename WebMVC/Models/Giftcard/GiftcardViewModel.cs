c55e8f6d4ab74ef7e9dc4f0987ef04d844fb5149
Adam Kala <xkala@fi.muni.cz>
2025-11-29 17:22:19 +0100

    Added giftcard view models.

---
+++ WebMVC/Models/Giftcard/GiftcardViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WebMVC.Models.Giftcard
+{
+    public class GiftcardViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public decimal Amount { get; set; }
+
+        public DateTime ValidFrom { get; set; }
+
+        public DateTime ValidTo { get; set; }
+
+        public int TotalCodes { get; set; }
+
+        public int UsedCodes { get; set; }
+    }
+}
