9731f7bdb9b3921585262e42c77a63259ebb72f6
Adam Kala <xkala@fi.muni.cz>
2025-11-29 17:24:04 +0100

    Updated Order model with GiftcardCodeId.

--- DAL/Models/Order.cs
+++ DAL/Models/Order.cs
@@ -12,8 +12,25 @@ namespace DAL.Models
         [ForeignKey(nameof(UserId))]
         public virtual User User { get; set; }
 
+        public int? GiftcardCodeId { get; set; }
+
+        [ForeignKey(nameof(GiftcardCodeId))]
+        public virtual GiftcardCode? GiftcardCode { get; set; }
+
         public virtual IEnumerable<OrderItem> Items { get; set; }
 
         public PaymentStatus PaymentStatus { get; set; } = PaymentStatus.InProgress;
+
+        public decimal TotalPrice => Items?.Sum(i => i.Quantity * i.Book.Price) ?? 0;
+
+        public decimal FinalPrice
+        {
+            get
+            {
+                decimal discount = GiftcardCode?.Giftcard?.Amount ?? 0;
+                decimal result = TotalPrice - discount;
+                return result < 0 ? 0 : result;
+            }
+        }
     }
 }
