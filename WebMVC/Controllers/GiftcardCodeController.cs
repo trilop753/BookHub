9545fd9ee29e1528aaab6ad168f5a2b365e872fa
Adam Kala <adamkala07@gmail.com>
2025-12-04 13:30:15 +0100

    Added caching into GiftcardCodeController

--- WebMVC/Controllers/GiftcardCodeController.cs
+++ WebMVC/Controllers/GiftcardCodeController.cs
@@ -1,5 +1,7 @@
 ﻿using BL.Facades.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using WebMVC.Caching;
+using WebMVC.Constants;
 using WebMVC.Mappers;
 
 namespace WebMVC.Controllers
@@ -7,15 +9,20 @@ namespace WebMVC.Controllers
     public class GiftcardCodeController : Controller
     {
         private readonly IGiftcardFacade _giftcardFacade;
+        private readonly IAppCache _cache;
 
-        public GiftcardCodeController(IGiftcardFacade giftcardFacade)
+        public GiftcardCodeController(IGiftcardFacade giftcardFacade, IAppCache cache)
         {
             _giftcardFacade = giftcardFacade;
+            _cache = cache;
         }
 
         public async Task<IActionResult> Index(int giftcardId)
         {
-            var res = await _giftcardFacade.GetByIdAsync(giftcardId);
+            var res = await _cache.GetOrCreateAsync(
+                CacheKeys.GiftcardDetail(giftcardId),
+                () => _giftcardFacade.GetByIdAsync(giftcardId)
+            );
 
             if (res.IsFailed)
             {
